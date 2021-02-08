88cfac3917f4326cb30473deb9a10408471b928e
Razibul-Alam <Razibul-Alam@users.noreply.github.com>
2021-02-08 13:04:45 +0600

    new commit

--- index.js
+++ index.js
@@ -1,6 +1,37 @@
 
 
-fetch('https://www.themealdb.com/api/json/v1/1/categories.php')
+// fetch('https://www.themealdb.com/api/json/v1/1/categories.php')
+//     .then(res => res.json())
+//     .then(data => dataDisplay(data));
+
+// function dataDisplay(data) {
+    
+//     let divcontain = document.querySelector('.div');
+
+//     console.log(data)
+//     for (let index = 0; index < data.categories.length; index++) {
+//         const element = data.categories[index];
+//         console.log(element.strCategory);
+//         let div = document.createElement('div');
+//         div.className='div2'
+//         let h3 = document.createElement('h3');
+//         h3.innerText = element.strCategory;
+//         let p = document.createElement('p');
+//         p.innerText = element.strCategory;
+        
+//         div.appendChild(h3);
+//         div.appendChild(p);
+//         divcontain.appendChild(div)
+//     }
+
+// }
+
+const button =document.getElementById('submit');
+button.addEventListener('click',function(){
+    const search =document.getElementById('search');
+    //console.log(search.value)
+    if(search.value=String){
+        fetch('https://www.themealdb.com/api/json/v1/1/categories.php')
     .then(res => res.json())
     .then(data => dataDisplay(data));
 
@@ -25,3 +56,5 @@ function dataDisplay(data) {
     }
 
 }
+    }
+})
