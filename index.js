b1d84d7068a63bccadddf18f5de6cd34dfca9250
Razibul-Alam <Razibul-Alam@users.noreply.github.com>
2021-02-08 12:41:16 +0600

    new commit

--- index.js
+++ index.js
@@ -5,14 +5,23 @@ fetch('https://www.themealdb.com/api/json/v1/1/categories.php')
     .then(data => dataDisplay(data));
 
 function dataDisplay(data) {
-    let ul = document.getElementById('ul');
-    //console.log(data)
+    
+    let divcontain = document.querySelector('.div');
+
+    console.log(data)
     for (let index = 0; index < data.categories.length; index++) {
         const element = data.categories[index];
-        console.log(element.strCategory)
-        let li = document.createElement('li');
-        li.innerText = element.strCategory;
-        ul.appendChild(li);
+        console.log(element.strCategory);
+        let div = document.createElement('div');
+        div.className='div2'
+        let h3 = document.createElement('h3');
+        h3.innerText = element.strCategory;
+        let p = document.createElement('p');
+        p.innerText = element.strCategory;
+        
+        div.appendChild(h3);
+        div.appendChild(p);
+        divcontain.appendChild(div)
     }
 
 }
