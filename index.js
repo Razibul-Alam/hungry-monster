34ee25a8c7698e98566bc2d6cb1d8d0a426c4e9f
Razibul-Alam <Razibul-Alam@users.noreply.github.com>
2021-02-08 09:02:40 +0600

    new commit

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
 
-fetch('https://www.themealdb.com/api/json/v1/1/categories.php')
-.then(res=>res.json())
-.then(data=>dataDisplay(data));
 
-function dataDisplay(data){
-data.categories.forEach(element => {
-    console.log(element.strCategory)
-})
+fetch('https://www.themealdb.com/api/json/v1/1/categories.php')
+    .then(res => res.json())
+    .then(data => dataDisplay(data));
+
+function dataDisplay(data) {
+    let ul = document.getElementById('ul');
+    //console.log(data)
+    for (let index = 0; index < data.categories.length; index++) {
+        const element = data.categories[index];
+        console.log(element.strCategory)
+        let li = document.createElement('li');
+        li.innerText = element.strCategory;
+        ul.appendChild(li);
+    }
+
 }
